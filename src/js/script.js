d1869be5b7c0cac16ac2d3e79914313183ef022a
Uceko <yurev.alesha@gmail.com>
2023-03-16 23:54:25 +0300

    js

---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+var name = "Ivan";
+let number = 7;
+const pi = 3.14;
+// Number 
+// let answer = prompt ("Вам есть 18 лет?","");
+// console.log (answer);
+
+// if (2*4 == 8*8) {// 
+//     console.log ('Верно')
+// } else {
+//     console.log ('Ошибка')
+// }
+
+let answer = confirm ('Вам есть 19?')
+if (answer) {
+    console.log ('проходи')
+} else {
+    cosole.log ('уходи')
+}
+
+if (num < 49) {
+    console.log ('Неправильно')
+} else if (num > 100) {
+    console.log ('Много')
+} else {
+    console.log('Верно')
+}
